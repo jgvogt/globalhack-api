2c47f2b622e91a46fbc730ce00d1b36e4957c410
Jerry Vogt <jerry.g.vogt@charter.com>
2018-10-13 10:28:09 -0500

    Refactored Added Ambassadors Tags

--- src/main/java/com/antipattrn/ambassador/entity/Ambassador.java
+++ src/main/java/com/antipattrn/ambassador/entity/Ambassador.java
@@ -1,41 +1,78 @@
 package com.antipattrn.ambassador.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "ambassadors")
 public class Ambassador {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private String id;
-    private String name;
-    private String description;
+
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    @Column(name = "POSTALCODE")
+    private String postalCode;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            })
+    @JoinTable(name = "ambassador_tags",
+            joinColumns = { @JoinColumn(name = "ambassador_id") },
+            inverseJoinColumns = { @JoinColumn(name = "tag_id") })
+    private Set<Tag> tags = new HashSet<>();
 
     public Ambassador() {
         //No Arg Constructor
     }
 
-    public Ambassador(String name, String description) {
-        this(null, name, description);
+    public Ambassador(String id, String firstName, String lastName, String postalCode) {
+        this(firstName, lastName, postalCode);
+        this.id = id;
     }
 
-    public Ambassador(String id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
+    public Ambassador(String firstName, String lastName, String postalCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.postalCode = postalCode;
     }
 
     public String getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
     }
 }
