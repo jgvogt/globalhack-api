adefc5a589f26a57c24667353fd5bc62417af10f
Andrew Scalise <paradigmcomplex@gmail.com>
2018-10-13 10:10:54 -0500

    added request mapping

--- src/main/java/com/antipattrn/ambassador/controller/TagController.java
+++ src/main/java/com/antipattrn/ambassador/controller/TagController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/api/tags")
 public class TagController {
     @Autowired
     private TagRepository tagRepository;
