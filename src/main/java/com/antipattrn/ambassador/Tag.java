fde7f151071735f063992b36b088b3a25b761dfd
Jerry Vogt <jerry.g.vogt@charter.com>
2018-10-13 09:27:46 -0500

    Added Tag Entity

---
+++ src/main/java/com/antipattrn/ambassador/Tag.java
@@ -0,0 +1,41 @@
+package com.antipattrn.ambassador;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private String id;
+    private String type;
+    private String name;
+
+    public Tag() {
+        //No Arg Constructor
+    }
+
+    public Tag(String type, String name) {
+        this(null, type, name);
+    }
+
+    public Tag(String id, String type, String name) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
