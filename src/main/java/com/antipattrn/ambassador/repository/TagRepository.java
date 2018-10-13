2881854c8c0b7e0bf3455d8c1fa88b24cdda1da8
Andrew Scalise <paradigmcomplex@gmail.com>
2018-10-13 12:59:35 -0500

    repository annotation added

--- src/main/java/com/antipattrn/ambassador/repository/TagRepository.java
+++ src/main/java/com/antipattrn/ambassador/repository/TagRepository.java
@@ -2,9 +2,11 @@ package com.antipattrn.ambassador.repository;
 
 import com.antipattrn.ambassador.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, String> {
     List<Tag> findByNameIgnoreCaseContaining(String name);
     List<Tag> findByTypeIgnoreCaseContaining(String type);
